a1bbce582f491010cdc1842db8b788d050cc6c00
Aman Eleven <hackerr647@gmail.com>
2021-10-28 10:47:42 +0530

    Create Weather Observation Station 10.sql

---
+++ Basic_Select/Weather Observation Station 10.sql
@@ -0,0 +1,10 @@
+select distinct(city) from station where not(
+city like '%a' or
+city like '%e' or
+city like '%i' or
+city like '%o' or
+city like '%u');
+
+
+//Author: AmanEleven
+//Follow: instagram.com/amaneleven
