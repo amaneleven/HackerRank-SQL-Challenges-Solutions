a075d5ef6fcba1e44b8d17fb1fd16f7bb20baec1
Aman Eleven <hackerr647@gmail.com>
2021-11-09 09:44:27 +0530

    Create Type of Triangle.sql

---
+++ Advanced Select/Type of Triangle.sql
@@ -0,0 +1,20 @@
+SELECT
+  CASE
+    WHEN (A + B <= C)
+    OR (B + C <= A)
+    OR (A + C <= B) THEN 'Not A Triangle'
+    WHEN (A = B)
+    AND (B = C) THEN 'Equilateral'
+    WHEN ((A = B) &(A != C))
+    OR ((B = C) &(B != A))
+    OR ((A = C) &(A != B)) THEN 'Isosceles'
+    WHEN (A != B)
+    AND (B != C)
+    AND (A != C) THEN 'Scalene'
+  END AS Triangle_Type
+FROM
+  TRIANGLES;
+  
+  
+//Author : AmanEleven
+//Follow : All Personal and Professional Links Here -->  https://linktr.ee/amaneleven
